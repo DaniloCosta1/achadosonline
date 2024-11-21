7a573020859c94c4f7671b01292f8eaa93ecebeb
Danilo Costa <99129932+DaniloCosta1@users.noreply.github.com>
2024-11-21 09:13:36 -0300

    ok

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ telaAmazon.innerHTML += ``;
 for(let item = 0; item <= produtos.length; item ++){
     if(produtos[item].plataforma.includes("amazon") ){
         telaAmazon.innerHTML += 
-            `<div class="link">
+            `<a class="link" href="${produtos[item].link}"_blank">
                 <h5>${produtos[item].nome}</h5>
-                <img src="${produtos[item].imagem}" alt="">
-                <a href="${produtos[item].link}">COMPRE AQUI</a>   
-            </div>`
+                <img src="${produtos[item].imagem}" alt="">     
+            </a>`
     }
     
 }
